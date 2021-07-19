8a44f4e61236f1b76fff5fdc243fb434a13aa286
nandan-shah <nandan2583@gmail.com>
2021-07-19 20:32:18 +0530

    search placeholder change

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
       <input
         type='text'
         className='search'
-        placeholder='Search.....'
+        placeholder='Enter city name'
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         onKeyPress={search}
